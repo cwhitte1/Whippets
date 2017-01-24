2b68783ddf55a6afbc889a591002f4e9e9361a33
Marcus Whitten <cwhitte1@trinity.edu>
2017-01-24 11:51:41 -0600

    Adding Game

---
+++ Assets/Scripts/LeafScript.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LeafScript : MonoBehaviour {
+    private bool success = false;
+    public Text winText;
+    public Canvas myCanvas;
+    public Canvas loseMessage;
+    public Collider2D floor;
+    public Text timeText;
+    public float targetTime = 10.0f;
+
+    // Use this for initialization
+    void Start () {
+        winText.text = "";
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (targetTime > 0)
+        {
+            timeText.text = targetTime.ToString("N4");
+        }else
+        {
+            timeText.text = "0.0000";
+        }
+        if (success)
+        {
+            success = true;
+            transform.position = new Vector2(0, 10);
+            //Destroy(gameObject);
+            myCanvas.gameObject.SetActive(true);
+        }
+
+        targetTime -= Time.deltaTime;
+        Debug.Log(targetTime);
+        if (targetTime <= 0.0f && success==false)
+        {
+            loseMessage.gameObject.SetActive(true);
+            DestroyFloor(floor);
+        }
+	
+	}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.layer == LayerMask.NameToLayer("Success"))
+        {
+            success = true;
+        }
+    }
+
+    void DestroyFloor(Collider2D other)
+    {
+        Destroy(other);
+    }
+
+}
---
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,50 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class PlayerMovementScript : MonoBehaviour {
+
+	public float walkSpeed = 3f;
+	public float jumpPower = 300f;
+
+	public LayerMask groundMask;
+
+	private Rigidbody2D theRigidBody;
+	private bool dead = false;
+	//private Animator animationController;
+
+	// Use this for initialization
+	void Start () {
+		theRigidBody = GetComponent<Rigidbody2D>();
+		//animationController = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		//animationController.SetFloat("speed", Mathf.Abs(theRigidBody.velocity.x));
+
+		if (dead)
+		{
+			theRigidBody.velocity = new Vector2(0, 0);
+			transform.position = new Vector2(0, 4);
+			dead = false;
+		}
+		else
+		{
+			float inputX = Input.GetAxis("Horizontal");
+			theRigidBody.velocity = new Vector2(inputX * walkSpeed, theRigidBody.velocity.y);
+            Debug.Log(inputX);
+			bool jumping = Input.GetButtonDown("Jump");
+		}
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.gameObject.layer == LayerMask.NameToLayer("Death"))
+		{
+			dead = true;
+		}
+	}
+}
